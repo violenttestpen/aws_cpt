d4c97da6623b6531a34e97154a0609f36627f1cf
violenttestpen <violenttestpen@users.noreply.github.com>
2023-06-12 08:39:02 +0800

    Implement `who_has` subcommand

--- aws_cpt/__main__.py
+++ aws_cpt/__main__.py
@@ -7,6 +7,7 @@ from aws_cpt.modules import (
     gather_assume_role,
     gather_permissions_for_role,
     gather_privesc,
+    gather_who_has,
 )
 
 
@@ -15,69 +16,39 @@ def main():
 
     cmd_parsers = parser.add_subparsers(dest="cmd", required=True)
 
-    assume_role_parser = cmd_parsers.add_parser("assume_role")
-    assume_role_parser.add_argument(
-        "--input",
-        "-i",
-        help="The filepath to the output of `iam get-account-authorization-details`",
-    )
-    assume_role_parser.add_argument(
-        "--output",
-        "-o",
-        help="The filepath to save the results",
-    )
-    assume_role_parser.add_argument(
-        "--verbose",
-        "-v",
-        default=False,
-        action="store_true",
-        help="Verbose mode",
-    )
+    parsers = {}
+    for cmd in ("assume_role", "permission", "privesc", "who_has"):
+        subparser = cmd_parsers.add_parser(cmd)
+        subparser.add_argument(
+            "--input",
+            "-i",
+            help="The filepath to the output of `iam get-account-authorization-details`",
+        )
+        subparser.add_argument(
+            "--output",
+            "-o",
+            help="The filepath to save the results",
+        )
+        subparser.add_argument(
+            "--verbose",
+            "-v",
+            default=False,
+            action="store_true",
+            help="Verbose mode",
+        )
+        parsers[cmd] = subparser
 
-    permission_parser = cmd_parsers.add_parser("permission")
-    permission_parser.add_argument(
-        "--input",
-        "-i",
-        help="The filepath to the output of `iam get-account-authorization-details`",
-    )
-    permission_parser.add_argument(
-        "--output",
-        "-o",
-        help="The filepath to save the results",
-    )
-    permission_parser.add_argument(
-        "--verbose",
-        "-v",
-        default=False,
-        action="store_true",
-        help="Verbose mode",
-    )
-    permission_parser.add_argument(
+    parsers["permission"].add_argument(
         "--reverse",
         "-r",
         default=False,
         action="store_true",
         help="Show actions grouped by resources instead",
     )
-    permission_parser.add_argument("role", help="Roles that are in scope")
+    parsers["permission"].add_argument("role", help="Roles that are in scope")
 
-    privesc_parser = cmd_parsers.add_parser("privesc")
-    privesc_parser.add_argument(
-        "--input",
-        "-i",
-        help="The filepath to the output of `iam get-account-authorization-details`",
-    )
-    privesc_parser.add_argument(
-        "--output",
-        "-o",
-        help="The filepath to save the results",
-    )
-    privesc_parser.add_argument(
-        "--verbose",
-        "-v",
-        default=False,
-        action="store_true",
-        help="Verbose mode",
+    parsers["who_has"].add_argument(
+        "permission", nargs="+", help="Filter using specified permission(s)"
     )
 
     args = parser.parse_args()
@@ -88,6 +59,8 @@ def main():
         gather_permissions_for_role.main(args)
     elif args.cmd == "privesc":
         gather_privesc.main(args)
+    elif args.cmd == "who_has":
+        gather_who_has.main(args)
 
 
 if __name__ == "__main__":
---
+++ aws_cpt/modules/gather_who_has.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import sys
+from itertools import chain
+from pathlib import Path
+from typing import Any, List
+
+from rich.console import Console, Group
+from rich.tree import Tree
+
+from aws_cpt.iam_structs import in_glob
+from aws_cpt.modules import create_new_argparser, gather_permissions_for_role
+
+
+def parse_permissions(data: Any, verbose=False):
+    roles, policies = gather_permissions_for_role.parse_iam(data)
+
+    for role in roles:
+        attached_policies_arns = [p["PolicyArn"] for p in role.AttachedManagedPolicies]
+        attached_policies = (p for p in policies if p.Arn in attached_policies_arns)
+
+        attached_policies_docs = (
+            p.Document
+            for sp in attached_policies
+            for p in sp.PolicyVersionList
+            if p.IsDefaultVersion
+        )
+        attached_policies_stmt = (p.Statement for p in attached_policies_docs)
+
+        inline_policies_stmt = (
+            s for p in role.RolePolicyList for s in p.PolicyDocument.Statement
+        )
+
+        statements = chain(*attached_policies_stmt, inline_policies_stmt)
+        yield role, gather_permissions_for_role.process_iam_actions(statements, verbose)
+
+
+def main(args: List[str]):
+    input_io = Path(args.input).open("rb") if args.input else sys.stdin.buffer
+    output_io = Path(args.output).open("w") if args.output else sys.stdout
+
+    data = json.load(input_io)
+    input_io.close()
+
+    root = Tree(f"Roles with any of Permissions: ({', '.join(args.permission)})")
+    for i, action in {0: "Allow", 1: "Deny"}.items():
+        action_node = None
+        for role, perms in parse_permissions(data, args.verbose):
+            role_node = None
+            for perm, rsrc in perms[i].items():
+                if not in_glob(args.permission, perm):
+                    continue
+
+                if action_node is None:
+                    action_node = root.add(action)
+
+                if role_node is None:
+                    role_node = action_node.add(role.RoleName)
+
+                if args.verbose:
+                    perm_node = role_node.add(perm)
+                    perm_node.add("Resource:").add(Group(*rsrc))
+
+    Console(file=output_io).print(root)
+
+
+if __name__ == "__main__":
+    parser = create_new_argparser()
+    parser.add_argument(
+        "permission", nargs="+", help="Filter using specified permission(s)"
+    )
+    args = parser.parse_args()
+
+    main(args)
